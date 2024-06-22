5ddab43725bbc31dd434fce91d1294edee03bc43
Maayan Zahavi <maayan.zahavi@hotmail.co.il>
2024-06-22 13:35:22 +0300

    In prorgess of getting videos from server

--- part2/src/components/HomePage/HomePage.js
+++ part2/src/components/HomePage/HomePage.js
@@ -1,22 +1,41 @@
-// Homepage component present the home page (inclding a navbar and videos)
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import VideoCollection from '../VideoCollection/VideoCollection'; 
 import './HomePage.css';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-const HomePage = ({ videos, users, user, setUser, setVideos, isDarkMode, setIsDarkMode }) => {
-  
-  const [videoList, setVideoList] = useState(videos);
+const HomePage = ({ users, user, setUser, setVideos, isDarkMode, setIsDarkMode }) => {
+  const [videoList, setVideoList] = useState([]);
   const navigate = useNavigate();
-  const doSearch = function(query) {
-    setVideoList(videos.filter((video) => video.title.includes(query)));
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const res = await fetch('http://localhost:8200/api/videos', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await res.json();
+        setVideoList(data);
+        setVideos(data);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
+
+    fetchVideos();
+  }, [setVideos]);
+
+  const doSearch = (query) => {
+    setVideoList(videoList.filter((video) => video.title.includes(query)));
     navigate('/home');
   };
 
   return (
     <div className="main-screen">
-      <Navbar user={user} setUser={setUser} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} doSearch={doSearch}/>
+      <Navbar user={user} setUser={setUser} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} doSearch={doSearch} />
       <div className="main-content">
         <div className="video-content">
           <VideoCollection videos={videoList} users={users} setVideos={setVideos} />
